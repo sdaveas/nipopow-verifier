93c43f7467200a929ced87940742f3cf5ce4ede4
Stelios Daveas <sdaveas@iit.demokritos.gr>
2019-12-05 17:15:48 +0200

    Expand unit test

--- unit_tests/test.sol
+++ unit_tests/test.sol
@@ -1,7 +1,14 @@
 pragma solidity ^0.5.13;
 
 contract Crosschain {
-  function test(bool value) public view returns(bool) {
-    return !value;
-  }
+    function test(bool value) public view returns(bool) {
+        return !value;
+    }
+
+    function payable_test(uint required_value) public payable returns(bool) {
+        if (msg.value > required_value) {
+            return true;
+        }
+        return false;
+    }
 }
--- unit_tests/unit_tests.py
+++ unit_tests/unit_tests.py
@@ -7,33 +7,61 @@ import test_contracts
 import unittest
 
 gas_block_limit = 3141592000
-contract_path = './test.sol'
+test_contract_path = './test.sol'
+nipopow_contract_path = '../contractNipopow.sol'
 
 print(gas_block_limit)
 
-chain = test_contracts.create_chain(gas_block_limit)
-ci = contract_interface.ContractInterface(chain, contract_path)
+test_chain = test_contracts.create_chain(gas_block_limit)
+test_ci = contract_interface.ContractInterface(test_chain, test_contract_path)
+
+nipopow_chain = test_contracts.create_chain(gas_block_limit)
+nipopow_ci = contract_interface.ContractInterface(nipopow_chain, nipopow_contract_path)
 
 class TestSolidityVersion(unittest.TestCase):
 
     def test_solc_version(self):
-        self.assertEqual(ci.solc_version ,'v0.5.13')
+        self.assertEqual(test_ci.solc_version ,'v0.5.13')
 
 class TestContractInterface(unittest.TestCase):
 
     def test_contracts_instances(self):
-        # Assert that the contract was compiled and deployed
-        self.assertEqual(len(ci.get_contracts()), 1)
+        self.assertEqual(len(test_ci.get_contracts()), 1)
 
 class TestContract(unittest.TestCase):
 
-    def test_function_gas(self):
-        # test setimate gas
-        contract = ci.get_contract();
+    def test_function_estemated_gas(self):
+        contract = test_ci.get_contract();
         gas = contract.functions.test(True).estimateGas()
         self.assertTrue(gas < gas_block_limit)
-        # test call
+
+    def test_function_used_gas(self):
+        contract = test_ci.get_contract();
+        tx = contract.functions.test(True).transact()
+        receipt = test_ci.w3.eth.getTransactionReceipt(tx)
+        gas_used = receipt['gasUsed']
+        self.assertTrue(gas_used < gas_block_limit)
+
+    def test_function_call(self):
+        contract = test_ci.get_contract();
         in_value = False
         res = contract.functions.test(in_value).call()
-        # test_contract.test(bool) reverts the input
-        self.assertEqual(res, not in_value)
+        self.assertTrue(not in_value)
+
+    def test_payable_function(self):
+        contract = test_ci.get_contract();
+        res = contract.functions.payable_test(4).call({'value':5})
+        self.assertTrue(res)
+        res = contract.functions.payable_test(6).call({'value':5})
+        self.assertFalse(res)
+        print(res)
+
+
+class TestSubmitProof(unittest.TestCase):
+
+    def test_submit_proof(self):
+        contract = nipopow_ci.get_contract();
+        proof = test_contracts.create_proof(blocks=45000)
+        res = test_contracts.submit_event_proof(contract, proof)
+        self.assertTrue(res)
+
