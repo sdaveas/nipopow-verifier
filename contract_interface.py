4735b240240f76d61a67890b4ebca57917629848
Stelios Daveas <sdaveas@iit.demokritos.gr>
2019-12-18 00:25:23 +0200

    Use previous solc version for benchmarking

--- contract_interface.py
+++ contract_interface.py
@@ -15,10 +15,10 @@ class ContractInterface:
     def __init__(
             self,
             contract_path_list=[],
-            solc_version='v0.5.13',
+            solc_version='v0.5.4',
             contract_instances=[],
             backend='ganache',
-            genesis_overrides={},
+            genesis_overrides={'gas_limit':3141592000},
             precompiled_contract={},
             ):
 
