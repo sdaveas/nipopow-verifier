ca51ea41150d374051acfab907f99856cc5ef4fc
sdaveas <sdaveas@gmail.com>
2020-03-13 20:54:18 +0200

    Fix prover to include genesis hash at the higher level of all interlinks

--- nipopow_verifier/tools/proof/create_blockchain_new.py
+++ nipopow_verifier/tools/proof/create_blockchain_new.py
@@ -5,6 +5,8 @@ _SelectCoreParams('regtest')
 from bitcoin.core import coreparams
 from tqdm import tqdm
 
+genesis = b"\xf3\x0cF\xbe\xc5B\xa3t9\x95.\x0f\xfe\x0ea\r\xbb\xcd\xad\x97\xee\xd9\xba\x94U\xdc\x90\x05\xcbW\xfcR"
+
 ###
 # Block header object
 ###
@@ -159,6 +161,15 @@ def mine_block(hashPrevBlock=b'\xbb'*32, nBits=0x207fffff, vInterlink=[], hashMe
             continue
     return header
 
+# Unfolds the tuple and puts genesis inside
+def add_genesis_to_interlink(interlink, genesis):
+    if interlink == (genesis, ()):
+        return (genesis, ())
+    if interlink == ():
+        return (genesis, ())
+    f, s = interlink
+    return (f, add_genesis_to_interlink(s, genesis))
+
 def create_blockchain(genesis=None, blocks=450000, headerMap=None, mapInterlink=None, hashMerkleRoot=b'\x00'*32):
     # This way of handling the mapInterlink only requires O(N) space
     # Rather than O(N log N) when done naively
@@ -174,6 +185,7 @@ def create_blockchain(genesis=None, blocks=450000, headerMap=None, mapInterlink=
 
     header = None
     for i in tqdm(range(blocks), desc='Creating blockchain'):
+        listInterlink = add_genesis_to_interlink(listInterlink, genesis.GetHash())
         vInterlink = list_flatten(listInterlink)
         if header is None:
             header = genesis
@@ -229,7 +241,8 @@ def make_proof(header, headerMap, mapInterlink, m=15, k=15):
             mu += 1
 
         # Nothing else at current level
-        if mu >= len(vInterlink):
+        # The last level is the genesis hash
+        if mu >= len(vInterlink) - 1:
             break
 
         # Skip to the next block at current level
@@ -239,6 +252,15 @@ def make_proof(header, headerMap, mapInterlink, m=15, k=15):
         verify_interlink(header.GetHash(), hash_interlink(vInterlink), mp)
 
         vInterlink = list_flatten(mapInterlink[header.GetHash()])
+
+    # Iterate the rest of the proof at the maximum level of each block until genesis
+    while header.GetHash() != genesis:
+        genesis_level = len(list_flatten(mapInterlink[header.GetHash()])) - 1
+        mp = prove_interlink(vInterlink, genesis_level)
+        header = headerMap[vInterlink[-1]]
+        verify_interlink(header.GetHash(), hash_interlink(vInterlink), mp)
+        vInterlink = list_flatten(mapInterlink[header.GetHash()])
+        proof.append((header.serialize(), mp))
 
     for hs_mp in proof:
         hs, mp = hs_mp
