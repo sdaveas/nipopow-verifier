aa3ae7d5a059395936f16c6d55b9575527cecd24
sdaveas <sdaveas@gmail.com>
2020-03-28 15:04:19 +0000

    Add python test to MMRs

---
+++ tests/mmr/mmr.py
@@ -0,0 +1,49 @@
+"""
+run with
+$
+"""
+
+import sys
+
+sys.path.append("../../tools/interface/")
+import contract_interface
+
+sys.path.append("..")
+from config import profiler
+
+def deploy(constructor_arguments=[]):
+    contract_path = "./MMR.sol"
+    interface = contract_interface.ContractInterface(
+        contract_path, backend="ganache", constructor_arguments=constructor_arguments
+    )
+    return interface
+
+def finalize(interface):
+    interface.end()
+
+def call(interface, function_name, function_args=[]):
+    """
+    Deploys the contract with contructor_arguments and runs a function with function_args
+    """
+
+    my_contract = interface.get_contract()
+    from_address = interface.w3.eth.accounts[0]
+    function = my_contract.get_function_by_name(function_name)(*function_args)
+    res = function.call({"from": from_address})
+    tx_hash = function.transact({"from": from_address})
+    receipt = interface.w3.eth.waitForTransactionReceipt(tx_hash)
+    interface.run_gas_profiler(profiler, tx_hash, function_name)
+    return {"result": res, "gas": receipt["gasUsed"]}
+
+data = b'\xaa'*32
+times = 10
+
+interface = deploy()
+
+print("Appending ", data.hex(), times, "times")
+result = call(interface, "testMMR", function_args=[data, times])
+print("MMR root:", result['result'].hex())
+print("Gas used:", result['gas'])
+
+finalize(interface)
+
