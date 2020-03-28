cc8f6b58678251308d2905080ccd01f73d364067
sdaveas <sdaveas@gmail.com>
2020-03-28 18:14:30 +0000

    Print debug events

--- tests/mmr/mmr.py
+++ tests/mmr/mmr.py
@@ -23,6 +23,20 @@ def deploy(constructor_arguments=[]):
 def finalize(interface):
     interface.end()
 
+def print_debug_events(contract, receipt):
+    try:
+        debug_events = contract.events.debug().processReceipt(receipt)
+    except Exception as ex:
+        debug_events = {}
+    if len(debug_events) > 0:
+        for e in debug_events:
+            log = dict(e)["args"]
+            if isinstance(log["value"], bytes):
+                value = log["value"].hex()
+            else:
+                value = log["value"]
+            print(log["tag"], "\t", value)
+    return debug_events
 
 def call(interface, function_name, function_args=[]):
     """
@@ -36,6 +50,7 @@ def call(interface, function_name, function_args=[]):
     tx_hash = function.transact({"from": from_address})
     receipt = interface.w3.eth.waitForTransactionReceipt(tx_hash)
     interface.run_gas_profiler(profiler, tx_hash, function_name)
+    print_debug_events(contract, receipt)
     return {"result": res, "gas": receipt["gasUsed"]}
 
 
