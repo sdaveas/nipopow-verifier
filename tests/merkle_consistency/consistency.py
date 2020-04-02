d33968c26c36f14d518e96d35ddfb607da60b5b5
sdaveas <sdaveas@gmail.com>
2020-04-03 00:01:06 +0300

    Call functions

--- tests/merkle_consistency/consistency.py
+++ tests/merkle_consistency/consistency.py
@@ -50,3 +50,19 @@ def print_debug_events(contract, receipt):
                 value = log["value"]
             print(log["tag"], "\t", value)
     return debug_events
+
+
+def call(interface, function_name, function_args=[]):
+    """
+    Runs a function with function_args
+    """
+
+    contract = interface.get_contract()
+    from_address = interface.w3.eth.accounts[0]
+    function = contract.get_function_by_name(function_name)(*function_args)
+    res = function.call({"from": from_address})
+    tx_hash = function.transact({"from": from_address})
+    receipt = interface.w3.eth.waitForTransactionReceipt(tx_hash)
+    interface.run_gas_profiler(profiler, tx_hash, function_name)
+    print_debug_events(contract, receipt)
+    return {"result": res, "gas": receipt["gasUsed"]}
