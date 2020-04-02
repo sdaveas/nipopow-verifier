e512fc4e93193ab6ecd31632f75ab2046108eed6
sdaveas <sdaveas@gmail.com>
2020-04-02 23:56:45 +0300

    Function for closest power to number

--- tests/merkle_consistency/consistency.sol
+++ tests/merkle_consistency/consistency.sol
@@ -3,4 +3,16 @@ pragma solidity ^0.6.0;
 
 contract consistency {
 
+    function closestPow2(uint256 number) public pure returns (uint256) {
+        uint256 closest = 1;
+        while ((closest << 1) <= number) {
+            closest <<= 1;
+        }
+        require(closest & (closest - 1) == 0, "Not power of 2");
+        if (closest == number) {
+            closest /= 2;
+        }
+        return closest;
+    }
+
 }
