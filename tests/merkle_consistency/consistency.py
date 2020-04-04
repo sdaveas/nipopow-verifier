08ad3b066d1c030bdbae6e1059ea863f317d479a
sdaveas <sdaveas@gmail.com>
2020-04-04 23:44:03 +0300

    Remove mini tests

--- tests/merkle_consistency/consistency.py
+++ tests/merkle_consistency/consistency.py
@@ -199,13 +199,3 @@ def root_1_from_const_proof(proof, n0, n1):
     interface = deploy()
     result = call(interface, "root1FromConsProof", function_args=[proof, n0, n1])
     return result
-
-
-size = 7
-data = []
-for i in range(size):
-    data.append(int(i).to_bytes(32, "big"))
-for m in range(1, len(data)):
-    proof = cons_proof_sub(data, m)["result"]
-    root = root_1_from_const_proof(proof, m, len(data))["result"]
-    print(root.hex())
