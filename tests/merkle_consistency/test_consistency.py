49f744baf205dbb77828dfc9717fda469bf539c0
sdaveas <sdaveas@gmail.com>
2020-04-04 22:43:13 +0300

    Unit test for test_closest_pow_of_2

--- tests/merkle_consistency/test_consistency.py
+++ tests/merkle_consistency/test_consistency.py
@@ -34,3 +34,17 @@ def test_log2_ceiling(init_environment):
     assert log2_ceiling(6)["result"] == 3
     assert log2_ceiling(7)["result"] == 3
     assert log2_ceiling(8)["result"] == 4
+
+
+def test_closest_pow_of_2(init_environment):
+
+    assert closest_pow_of_2(1)["result"] == 0
+    assert closest_pow_of_2(2)["result"] == 1
+    assert closest_pow_of_2(3)["result"] == 2
+    assert closest_pow_of_2(4)["result"] == 2
+    assert closest_pow_of_2(5)["result"] == 4
+    assert closest_pow_of_2(6)["result"] == 4
+    assert closest_pow_of_2(7)["result"] == 4
+    assert closest_pow_of_2(8)["result"] == 4
+    assert closest_pow_of_2(9)["result"] == 8
+    assert closest_pow_of_2(10)["result"] == 8
