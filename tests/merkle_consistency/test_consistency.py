7126d63c8130ebd2b94ae1b6323b35940a6b9ed8
sdaveas <sdaveas@gmail.com>
2020-04-04 22:42:38 +0300

    Unit test for test_log2_ceiling

--- tests/merkle_consistency/test_consistency.py
+++ tests/merkle_consistency/test_consistency.py
@@ -21,3 +21,16 @@ def init_environment():
     data = []
     for i in range(size):
         data.append(int(i).to_bytes(32, "big"))
+
+
+def test_log2_ceiling(init_environment):
+
+    assert log2_ceiling(0)["result"] == 0
+    assert log2_ceiling(1)["result"] == 1
+    assert log2_ceiling(2)["result"] == 2
+    assert log2_ceiling(3)["result"] == 2
+    assert log2_ceiling(4)["result"] == 3
+    assert log2_ceiling(5)["result"] == 3
+    assert log2_ceiling(6)["result"] == 3
+    assert log2_ceiling(7)["result"] == 3
+    assert log2_ceiling(8)["result"] == 4
