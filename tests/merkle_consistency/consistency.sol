1d3b3a6108f38dbe76ba21fd48b8dab2e272a22f
sdaveas <sdaveas@gmail.com>
2020-04-05 18:30:37 +0300

    Remove merkleTreeHashRec

--- tests/merkle_consistency/consistency.sol
+++ tests/merkle_consistency/consistency.sol
@@ -39,11 +39,6 @@ contract Consistency {
         return subArray;
     }
 
-    // Returns the root of merkle tree as computed from data (recursive)
-    function merkleTreeHashRec(bytes32[] memory data) public returns (bytes32) {
-        uint256 n = data.length;
-        if (n == 1) {
-            return sha256(abi.encodePacked(uint256(0), data[0]));
     // Substitute for array[start:end] for bytes32[]
     function subArrayBytes32(bytes32[] memory array, uint256 start, uint256 end)
         public
@@ -56,27 +51,6 @@ contract Consistency {
         for (uint256 i = start; i < end; i++) {
             subArray[i - start] = array[i];
         }
-
-        uint256 k = closestPow2(n);
-
-        bytes32[] memory left = new bytes32[](k);
-        for (uint256 i = 0; i < k; i++) {
-            left[i] = data[i];
-        }
-
-        bytes32[] memory right = new bytes32[](n - k);
-        for (uint256 i = 0; i < n - k; i++) {
-            right[i] = data[i + k];
-        }
-
-        return
-            sha256(
-                abi.encodePacked(
-                    uint256(1),
-                    merkleTreeHashRec(left),
-                    merkleTreeHashRec(right)
-                )
-            );
         return subArray;
     }
 
