8e84b467e3233b501c5681af6e221c4146fd7103
sdaveas <sdaveas@gmail.com>
2020-04-10 17:41:21 +0300

    Update test

--- tests/test_disjoint_proofs.py
+++ tests/test_disjoint_proofs.py
@@ -72,16 +72,10 @@ def test_submit_disjoint_proofs(init_environment):
             pref = b"\xff"
         array_2.append(pref + i.to_bytes(31, "big"))
 
-    for i in range(size):
-        if i < lca:
-            assert array_1[i] != array_2[i]
-        else:
-            assert array_1[i] == array_2[i]
-
-    res = call(interface, "disjointProofs", [array_1, array_2, int(lca)])
+    res = call(interface, "disjointProofs", [array_1, array_2, lca])
     assert res["result"] == False
 
-    res = call(interface, "disjointProofsFixed", [array_1, array_2, int(lca)])
+    res = call(interface, "disjointProofsFixed", [array_1, array_2, lca])
     assert res["result"] == True
 
 
