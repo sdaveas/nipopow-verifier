698eafc55f551b5a0152cf5cf54caee088fd36e6
sdaveas <sdaveas@gmail.com>
2020-04-16 14:58:59 +0300

    Update fail on require

--- tests/test_fail_on_require.py
+++ tests/test_fail_on_require.py
@@ -9,29 +9,14 @@ sys.path.append("../tools/interface/")
 import contract_interface
 
 import pytest
-import web3
-from config import errors, extract_message_from_error, genesis
-from eth_tester.exceptions import TransactionNotFound
+from config import extract_message_from_error
 
 
-def test_fail_pyevm():
-    """
-    Test failure at require in smart contract for Py_EVM
-    """
-
-    contract_path = "./contracts/fail_on_require.sol"
-    interface = contract_interface.ContractInterface(
-        contract_path, backend="Py-EVM", constructor_arguments=genesis
+def make_interface(backend):
+    return contract_interface.ContractInterface(
+        contract={"path": "./contracts/fail_on_require.sol", "ctor": []},
+        backend=backend,
     )
-    my_contract = interface.get_contract()
-    from_address = interface.w3.eth.accounts[0]
-
-    with pytest.raises(Exception) as ex:
-        my_contract.functions.fail().call({"from": from_address})
-        tx_hash = my_contract.functions.fail().transact({"from": from_address})
-        interface.w3.eth.waitForTransactionReceipt(tx_hash)
-    interface.end()
-    assert ex.match("test failed successfully")
 
 
 def test_fail_geth():
@@ -39,18 +24,8 @@ def test_fail_geth():
     Test failure at require in smart contract for geth
     """
 
-    contract_path = "./contracts/fail_on_require.sol"
-    interface = contract_interface.ContractInterface(
-        contract_path, backend="geth", constructor_arguments=genesis
-    )
-    my_contract = interface.get_contract()
-    from_address = interface.w3.eth.accounts[0]
-
     with pytest.raises(Exception) as ex:
-        my_contract.functions.fail().call({"from": from_address})
-        tx_hash = my_contract.functions.fail().transact({"from": from_address})
-        interface.w3.eth.waitForTransactionReceipt(tx_hash)
-    interface.end()
+        make_interface("geth").call("fail")
     assert ex.match("test failed successfully")
 
 
@@ -59,17 +34,6 @@ def test_fail_ganache():
     Test failure at require in smart contract for ganache
     """
 
-    contract_path = "./contracts/fail_on_require.sol"
-    interface = contract_interface.ContractInterface(
-        contract_path, backend="ganache", constructor_arguments=genesis
-    )
-
-    my_contract = interface.get_contract()
-    from_address = interface.w3.eth.accounts[0]
-
     with pytest.raises(Exception) as ex:
-        my_contract.functions.fail().call({"from": from_address})
-        tx_hash = my_contract.functions.fail().transact({"from": from_address})
-        interface.w3.eth.waitForTransactionReceipt(tx_hash)
-    interface.end()
+        make_interface("ganache").call("fail")
     assert extract_message_from_error(ex) == "test failed successfully"
