1d28b1d18f5caccc3fe0fa7dae7e2a9b149f34c0
sdaveas <sdaveas@gmail.com>
2020-04-28 12:55:41 +0300

    Add tests for score

--- tests/test_level.py
+++ tests/test_level.py
@@ -7,13 +7,10 @@ import sys
 import pytest
 from tqdm import tqdm
 
-sys.path.append("../tools/interface/")
-import contract_interface
-
 sys.path.append("../tools/proof/")
 from proof import Proof
 from create_proof import ProofTool
-from create_blockchain_new import CBlockHeaderPopow
+from create_blockchain_new import CBlockHeaderPopow, Hash
 
 from contract_api import make_interface
 
@@ -27,10 +24,31 @@ def init_environment():
     global interface
     interface = make_interface("ganache")
 
+    mainblocks = 100
+    fork_index = 50
+    forkblocks = 100
+    pt = ProofTool("../data/proofs/")
+    (
+        proof_name,
+        fork_name,
+        lca,
+        fork_header,
+        fork_header_map,
+        fork_interlink_map,
+    ) = pt.create_proof_and_forkproof(mainblocks, fork_index, forkblocks)
+
     global proof
-    blocks = 100
     proof = Proof()
-    proof.set(ProofTool("../data/proofs/").fetch_proof(blocks))
+    proof.set(pt.fetch_proof_by_name(proof_name))
+
+    global fork
+    fork = Proof()
+    fork.set(
+        pt.fetch_proof_by_name(fork_name),
+        header=fork_header,
+        header_map=fork_header_map,
+        interlink_map=fork_interlink_map,
+    )
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -42,27 +60,49 @@ def finish_session(request):
     yield
     # you can access the session from the injected 'request':
     session = request.session
+    interface = make_interface("ganache")
     interface.end()
 
 
-def test_sufficient_collateral(init_environment):
-    """
-    Test contract call with sufficient collateral
-    """
+def test_level(init_environment):
 
-    from collections import defaultdict
-
-    levels = defaultdict(lambda: 0)
+    """
+    Test level computation
+    """
 
     for index in tqdm(range(proof.size)):
         hashed_header = proof.hashed_headers[index].hex()
         level_from_solidity = interface.call(
             "getLevel", function_args=[hashed_header]
         )["result"]
-
         header, _ = proof.proof[index]
         level_from_python = CBlockHeaderPopow.deserialize(
             header
         ).compute_level()
-
         assert level_from_solidity == level_from_python
+
+
+def test_scores(init_environment):
+
+    """
+    Test score computation
+    """
+
+    score_from_solidity = interface.call(
+        "bestArg", function_args=[proof.hashed_headers, proof.size]
+    )["result"]
+    assert score_from_solidity == proof.best_score
+
+    fork_hashed_headers = []
+    for (h, _) in fork.best_level_subproof:
+        fork_hashed_headers.append(Hash(h))
+    score_from_solidity = interface.call(
+        "bestArg",
+        function_args=[fork_hashed_headers, len(fork_hashed_headers)],
+    )["result"]
+    assert score_from_solidity == fork.best_score
+
+    score_from_solidity = interface.call(
+        "argAtLevel", function_args=[fork_hashed_headers, fork.best_level]
+    )["result"]
+    assert score_from_solidity == fork.best_score
