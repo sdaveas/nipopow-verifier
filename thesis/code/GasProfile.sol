47444c4c757280243b2e263b23fb9314288faaf8
sdaveas <sdaveas@gmail.com>
2020-06-14 21:23:55 +0300

    Edit gas profiler code

--- thesis/code/GasProfile.sol
+++ thesis/code/GasProfile.sol
@@ -1,7 +1,7 @@
 // Gas used by transaction: 183443
 0       pragma solidity ^0.6.6;
-0
 84      contract Contract {
+0
 0           uint s;
 0           address holder;
 0
@@ -13,7 +13,6 @@
 598             for (uint i=0; i<c; i++ ) {
 155069              s = i;
 0               }
-0
 226             if (s > 1) {
 5233                s += 1;
 0               }
